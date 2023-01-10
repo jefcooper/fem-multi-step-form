191d32a6fef000276e16cf69146a3dd1029b18a3
Jeff Cooper <jefcooper@gmail.com>
2023-01-09 21:45:29 -0500

    solved overflow by using grid only on mobile body

--- src/script.js
+++ src/script.js
@@ -2,4 +2,5 @@
  * script.js - root javascript file for the project, imports all others
  ****************************************************************************/
 
+import "/scripts/plan-term-toggle-switch.js";
 import "/scripts/wizard-form.js";
